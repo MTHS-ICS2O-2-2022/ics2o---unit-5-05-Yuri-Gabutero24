5b3145ed790fc80b2f1d6cab13eb60e003d529ce
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-05 17:24:05 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
